dd914ee93423e904586059826fe66c9ec8510193
natade-jp <natade3@gmail.com>
2017-05-02 15:49:16 +0900

    CSSを修正

--- docs/samples/SComponent/main.js
+++ docs/samples/SComponent/main.js
@@ -7,51 +7,57 @@
 	
 	System.out.println("HTML での部品用のクラスです。");
 	
-	var obj1, obj2, obj3, obj4, obj5;
+	var label1, label2, label3;
 	
 	// パネルを作って、指定した ID の要素内に入れる。
-	obj1 = new SLabel("SComponentPutType.IN");
-	obj1.putMe("id_test_block", SComponent.putype.IN);
+	label1 = new SLabel("SComponentPutType.IN");
+	label1.putMe("id_test_block", SComponent.putype.IN);
 	
 	// obj2 は、 obj1 の右に配置する
-	obj2 = new SLabel("SComponentPutType.RIGHT");
-	obj1.put(obj2, SComponent.putype.RIGHT);
+	label2 = new SLabel("SComponentPutType.RIGHT");
+	label1.put(label2, SComponent.putype.RIGHT);
 	
 	// obj3 は、 obj2 の下に配置する
-	obj3 = new SLabel("SComponentPutType.NEWLINE");
-	obj2.put(obj3, SComponent.putype.NEWLINE);
+	label3 = new SLabel("SComponentPutType.NEWLINE");
+	label2.put(label3, SComponent.putype.NEWLINE);
 	
 	// obj3 のサイズを指定する
-	obj3.setUnit(SComponent.unittype.EM);
-	obj3.setSize(30, 2);
-	System.out.println("width " + obj3.getWidth() + obj3.getUnit());
-	System.out.println("height " + obj3.getHeight() + obj3.getUnit());
+	label3.setUnit(SComponent.unittype.EM);
+	label3.setSize(30, 2);
+	System.out.println("width " + label3.getWidth() + label3.getUnit());
+	System.out.println("height " + label3.getHeight() + label3.getUnit());
 	
 	// obj1 の内容を変更する
-	obj1.setText("【" + obj1.getText() + "】");
+	label1.setText("【" + label1.getText() + "】");
 	
-	obj4 = new SButton("button1");
-	obj3.put(obj4, SComponent.putype.NEWLINE);
+	var button1, button2;
+	var filebutton1;
+	
+	button1 = new SButton("button1");
+	label3.put(button1, SComponent.putype.NEWLINE);
+	button2 = new SButton("button2");
+	button1.put(button2, SComponent.putype.RIGHT);
 	
 	// クリックすると内部の関数が呼ばれる
-	obj4.addOnClickFunction(function () {
+	button1.addOnClickFunction(function () {
 		pushed--;
 		System.out.println("pushed !");
-		obj4.setText("残り " + pushed);
+		button1.setText("残り " + pushed);
 		
 		// 押すたびに有効化／無効化の変更
-		obj5.setEnabled(!obj5.isEnabled());
+		button2.setEnabled(!button2.isEnabled());
+		filebutton1.setEnabled(!filebutton1.isEnabled());
 		
 		// 何回か押したら非表示にする
 		if(pushed === 0) {
-			obj5.setVisible(false);
+			filebutton1.setVisible(false);
 		}
 	});
 	
-	obj5 = new SFileButton("file");
-	obj5.setFileAccept(SFileButton.fileaccept.image);
-	obj4.put(obj5, SComponent.putype.NEWLINE);
-	obj5.addOnClickFunction(function(file) {
+	filebutton1 = new SFileButton("file");
+	filebutton1.setFileAccept(SFileButton.fileaccept.image);
+	button2.put(filebutton1, SComponent.putype.NEWLINE);
+	filebutton1.addOnClickFunction(function(file) {
 		var i = 0;
 		for(;i < file.length; i++) {
 			System.out.println(file[i].name + " " + file[i].size + "byte");
