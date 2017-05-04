83b473c14ea3b289f4b97df6564ab6db2eabd536
natade-jp <natade3@gmail.com>
2017-05-05 00:02:26 +0900

    単純な画像データを読み書きするクラスを追加

--- docs/library/SenkoLib/SImageProcessing.js
+++ docs/library/SenkoLib/SImageProcessing.js
@@ -1,4 +1,4 @@
-/* global System */
+/* global System, ImageData */
 
 ﻿/**
  * SImageProcessing.js
@@ -17,46 +17,149 @@
  * 色クラス
  * /////////////////////////////////////////////////////////
  */
-var SIPColor = function(color) {
-	this.rgba = color;
+var SIPColor = function() {
+};
+SIPColor.prototype.getColor = function() {
+	return null;
 };
 SIPColor.prototype.clone = function() {
-	return new SIPColor(this.rgba);
+	return null;
 };
-SIPColor.prototype.addScalar = function(x) {
+SIPColor.prototype.one = function() {
+	return null;
+};
+SIPColor.prototype.add = function() {
+	return null;
+};
+SIPColor.prototype.sub = function() {
+	return null;
+};
+SIPColor.prototype.mul = function() {
+	return null;
+};
+SIPColor.prototype.div = function() {
+	return null;
+};
+SIPColor.prototype.max = function() {
+	return null;
+};
+SIPColor.prototype.min = function() {
+	return null;
+};
+SIPColor.lerp = function(v0, v1, x) {
+	var delta = v1.sub(v0);
+	return v0.add(delta.mul(x));
+};
+SIPColor.lerpCosine = function(v0, v1, x) {
+	return this.lerp(v0, v1,((1.0 - Math.cos(Math.PI * x)) * 0.5));
+};
+SIPColor.lerpHermitian2p3 = function(v0, v1, x) {
+	return this.lerp(v0, v1, (x * x * (3.0 - 2.0 * x)));
+};
+SIPColor.lerpHermitian2p5 = function(v0, v1, x) {
+	return this.lerp(v0, v1, (x * x * x * (6.0 * x * x - 15.0 * x + 10.0)));
+};
+SIPColor.lerpHermitian4p = function(v0, v1, v2, v3, x) {
+	var P = v3.sub(v2).sub(v0.sub(v1));
+	var Q = v0.sub(v1).sub(P);
+	var R = v2.sub(v0);
+	var S = v1;
+	return  P.mul(x * x * x).add(Q.mul(x * x)).add(R.mul(x)).add(S);
+};
+var SIPColorScalar = function(color) {
+	this.x = color;
+};
+SIPColorScalar.prototype = new SIPColor();
+SIPColorScalar.prototype.getColor = function() {
+	return this.x;
+};
+SIPColorScalar.prototype.clone = function() {
+	return new SIPColorScalar(this.x);
+};
+SIPColorScalar.prototype.one = function() {
+	return new SIPColorScalar(1.0);
+};
+SIPColorScalar.prototype.add = function(x) {
 	var color = this.clone();
-	color.rgba[0] += x;
-	color.rgba[1] += x;
-	color.rgba[2] += x;
-	color.rgba[3] += x;
+	color.x += x;
 	return color;
 };
-SIPColor.prototype.addVector = function(v) {
+SIPColorScalar.prototype.sub = function(x) {
 	var color = this.clone();
-	color.rgba[0] += v[0];
-	color.rgba[1] += v[1];
-	color.rgba[2] += v[2];
-	color.rgba[3] += v[3];
+	color.x -= x;
 	return color;
 };
-SIPColor.prototype.mulScalar = function(x) {
+SIPColorScalar.prototype.mul = function(x) {
 	var color = this.clone();
-	color.rgba[0] *= x;
-	color.rgba[1] *= x;
-	color.rgba[2] *= x;
-	color.rgba[3] *= x;
+	color.x *= x;
 	return color;
 };
-SIPColor.prototype.mulVector = function(v) {
+SIPColorScalar.prototype.div = function(x) {
 	var color = this.clone();
-	color.rgba[0] *= v[0];
-	color.rgba[1] *= v[1];
-	color.rgba[2] *= v[2];
-	color.rgba[3] *= v[3];
+	color.x /= x;
 	return color;
 };
-SIPColor.prototype.mulMatrix = function(m) {
-	var color = new SIPColor();
+SIPColorScalar.prototype.max = function(c) {
+	var color = this.clone();
+	color.x = Math.max(c.x, this.x);
+	return color;
+};
+SIPColorScalar.prototype.min = function(c) {
+	var color = this.clone();
+	color.x = Math.min(c.x, this.x);
+	return color;
+};
+var SIPColorRGBA = function(color) {
+	this.rgba = color;
+};
+SIPColorRGBA.prototype = new SIPColor();
+SIPColorRGBA.prototype.getColor = function() {
+	return this.rgba;
+};
+SIPColorRGBA.prototype.clone = function() {
+	return new SIPColorRGBA(this.rgba);
+};
+SIPColorRGBA.prototype.one = function() {
+	return new SIPColorRGBA([1.0, 1.0, 1.0, 1.0]);
+};
+SIPColorRGBA.prototype.add = function(x) {
+	var color = this.clone();
+	color.rgba[0] += x;	color.rgba[1] += x;
+	color.rgba[2] += x;	color.rgba[3] += x;
+	return color;
+};
+SIPColorRGBA.prototype.sub = function(x) {
+	var color = this.clone();
+	color.rgba[0] -= x;	color.rgba[1] -= x;
+	color.rgba[2] -= x;	color.rgba[3] -= x;
+	return color;
+};
+SIPColorRGBA.prototype.addVector = function(v) {
+	var color = this.clone();
+	color.rgba[0] += v[0];	color.rgba[1] += v[1];
+	color.rgba[2] += v[2];	color.rgba[3] += v[3];
+	return color;
+};
+SIPColorRGBA.prototype.subVector = function(v) {
+	var color = this.clone();
+	color.rgba[0] -= v[0];	color.rgba[1] -= v[1];
+	color.rgba[2] -= v[2];	color.rgba[3] -= v[3];
+	return color;
+};
+SIPColorRGBA.prototype.mul = function(x) {
+	var color = this.clone();
+	color.rgba[0] *= x;	color.rgba[1] *= x;
+	color.rgba[2] *= x;	color.rgba[3] *= x;
+	return color;
+};
+SIPColorRGBA.prototype.mulVector = function(v) {
+	var color = this.clone();
+	color.rgba[0] *= v[0];	color.rgba[1] *= v[1];
+	color.rgba[2] *= v[2];	color.rgba[3] *= v[3];
+	return color;
+};
+SIPColorRGBA.prototype.mulMatrix = function(m) {
+	var color = new SIPColorRGBA();
 	color.rgba[0] =	this.rgba[0] * m[0][0] +
 					this.rgba[1] * m[0][1] +
 					this.rgba[2] * m[0][2] +
@@ -75,7 +178,19 @@ SIPColor.prototype.mulMatrix = function(m) {
 					this.rgba[3] * m[3][3];
 	return color;
 };
-SIPColor.prototype.max = function(c) {
+SIPColorRGBA.prototype.div = function(x) {
+	var color = this.clone();
+	color.rgba[0] /= x;	color.rgba[1] /= x;
+	color.rgba[2] /= x;	color.rgba[3] /= x;
+	return color;
+};
+SIPColorRGBA.prototype.divVector = function(v) {
+	var color = this.clone();
+	color.rgba[0] /= v[0];	color.rgba[1] /= v[1];
+	color.rgba[2] /= v[2];	color.rgba[3] /= v[3];
+	return color;
+};
+SIPColorRGBA.prototype.max = function(c) {
 	var color = this.clone();
 	color.rgba[0] = Math.max(c.color[0], this.rgba[0]);
 	color.rgba[1] = Math.max(c.color[1], this.rgba[1]);
@@ -83,7 +198,7 @@ SIPColor.prototype.max = function(c) {
 	color.rgba[3] = Math.max(c.color[3], this.rgba[3]);
 	return color;
 };
-SIPColor.prototype.min = function(c) {
+SIPColorRGBA.prototype.min = function(c) {
 	var color = this.clone();
 	color.rgba[0] = Math.min(c.color[0], this.rgba[0]);
 	color.rgba[1] = Math.min(c.color[1], this.rgba[1]);
@@ -94,61 +209,24 @@ SIPColor.prototype.min = function(c) {
 
 /**
  * /////////////////////////////////////////////////////////
- * 実際に、指定した部分の色を取得するクラス
- * 必ず、xとyは中に納まることを保証すること
- * /////////////////////////////////////////////////////////
- */
-var SIPColorPicker = function() {
-};
-SIPColorPicker.prototype.getColor = function(x, y) {
-	return new SIPColor(0, 0, 0, 0);
-};
-var SIPColorPickerContext = function(context) {
-	this.super		= SIPColorPicker.prototype;
-	this.context	= context;
-};
-SIPColorPickerContext.prototype = new SIPColorPicker();
-SIPColorPickerContext.prototype.getColor = function(x, y) {
-	var p = this.context.getImageData( x, y, 1, 1 ).data;
-	return new SIPColor([p[0], p[1], p[2], p[3]]);
-};
-var SIPColorPickerImageData = function(imagedata, width, height) {
-	this.super		= SIPColorPicker.prototype;
-	this.imagedata	= imagedata;
-	this.width		= width;
-	this.height		= height;
-};
-SIPColorPickerImageData.prototype = new SIPColorPicker();
-SIPColorPickerImageData.prototype.getColor = function(x, y) {
-	var p = (y * width + x) * 4;
-	[	this.imagedata[p],
-		this.imagedata[p + 1],
-		this.imagedata[p + 2],
-		this.imagedata[p + 3]
-	];
-};
-var SIPColorPickerText = function() {
-	this.super		= SIPColorPicker.prototype;
-};
-SIPColorPickerText.prototype = new SIPColorPicker();
-SIPColorPickerText.prototype.getColor = function(x, y) {
-	console.log("" + x + "," + y);
-};
-
-/**
- * /////////////////////////////////////////////////////////
  * 指定した部分の色を取得するクラス
  * xとyは中に納まらなくてもよいが、離散値を使用すること
+ * 
+ * SIPPixelSelecterNormal はみでた色は null
+ * SIPPixelSelecterFill   はみでた色は最も近い位置の色にする
+ * SIPPixelSelecterRepeat はみでた色は反対側の方向から取得する
  * /////////////////////////////////////////////////////////
  */
-
 var SIPPixelSelecter = function() {
 };
 SIPPixelSelecter.prototype._init = function(picker, width, height) {
-	if(!(picker instanceof SIPColorPicker)) {
-		throw "IllegalArgumentException";
-	}
+	this.setPicker(picker);
+	this.setSize(width, height);
+};
+SIPPixelSelecter.prototype.setPicker = function(picker) {
 	this.picker= picker;
+};
+SIPPixelSelecter.prototype.setSize = function(width, height) {
 	this.width  = width;
 	this.height = height;
 };
@@ -159,7 +237,7 @@ SIPPixelSelecter.prototype.getColor = function(x, y) {
 		return this.picker.getColor(x, y);
 	}
 	else {
-		return new SIPColor(0, 0, 0, 0);
+		return null;
 	}
 };
 var SIPPixelSelecterNormal = function(picker, width, height) {
@@ -208,6 +286,157 @@ SIPPixelSelecterRepeat.prototype.getColor = function(x, y) {
 
 /**
  * /////////////////////////////////////////////////////////
+ * 画像データクラス
+ * SIPDataRGBA   32bit整数 0xRRGGBBAA で管理
+ * SIPDataScalar 32bit浮動小数点で管理
+ * /////////////////////////////////////////////////////////
+ */
+
+var SIPData = function() {
+};
+SIPData.prototype.setSize = function(width, height) {
+	if((this.width === width) && (this.height === height)) {
+		return;
+	}
+	this.width	= width;
+	this.height	= height;
+	this.data	= new Uint8ClampedArray(this.width * this.height);
+};
+SIPData.prototype.clear = function() {
+	if(this.data) {
+		this.data.fill(0);
+	}
+};
+SIPData.prototype.getColor = function(x, y) {
+	var p = (y * this.width + x) * 4;
+	return new SIPColorRGBA([
+		this.data[p],
+		this.data[p + 1],
+		this.data[p + 2],
+		this.data[p + 3]
+	]);
+};
+SIPData.prototype.setColor = function(x, y, color) {
+	var p = (y * this.width + x) * 4;
+	this.data[p]     = color.getColor()[0];
+	this.data[p + 1] = color.getColor()[1];
+	this.data[p + 2] = color.getColor()[2];
+	this.data[p + 3] = color.getColor()[3];
+};
+SIPData.prototype.each = function(func) {
+	var x = 0, y = 0;
+	for(; y < height; y++) {
+		for(x = 0; x < width; x++) {
+			this.setColor(x, y, func(this.getColor(x, y)));
+		}
+	}
+};
+var SIPDataRGBA = function() {
+};
+SIPDataRGBA.prototype = new SIPData();
+SIPDataRGBA.prototype.putDataScalar = function(imagedata, n) {
+	this.setSize(imagedata.width, imagedata.height);
+	if(!n) {
+		n = 0;
+	}
+	var p = 0, i = 0;
+	for(; i < imagedata.data.length; i++) {
+		this.data[p + n] = Math.floor(imagedata.data[i]);
+		p += 4;
+	}
+};
+SIPDataRGBA.prototype.putDataScalarR = function(imagedata) {
+	this.putDataScalar(imagedata, 0);
+};
+SIPDataRGBA.prototype.putDataScalarG = function(imagedata) {
+	this.putDataScalar(imagedata, 1);
+};
+SIPDataRGBA.prototype.putDataScalarB = function(imagedata) {
+	this.putDataScalar(imagedata, 2);
+};
+SIPDataRGBA.prototype.putDataScalarA = function(imagedata) {
+	this.putDataScalar(imagedata, 3);
+};
+SIPDataRGBA.prototype.putImageData = function(imagedata) {
+	this.width	= imagedata.width;
+	this.height	= imagedata.height;
+	this.data   = new Uint8ClampedArray(imagedata.data);
+	this.data.set(imagedata.data);
+};
+SIPDataRGBA.prototype.getImageData = function() {
+	var canvas, context;
+	canvas = document.createElement("canvas");
+	canvas.width  = this.width;
+	canvas.height = this.height;
+	context = canvas.getContext("2d");
+	var imagedata = context.getImageData(0, 0, canvas.width, canvas.height);
+	imagedata.data.set(this.data);
+	return imagedata;
+};
+
+var SIPDataScalar = function() {
+};
+SIPDataScalar.prototype = new SIPData();
+SIPDataScalar.prototype.setSize = function(width, height) {
+	if((this.width === width) && (this.height === height)) {
+		return;
+	}
+	this.width	= width;
+	this.height	= height;
+	this.data	= new Float32Array(this.width * this.height);
+};
+SIPDataScalar.prototype.getColor = function(x, y) {
+	var p = y * this.width + x;
+	return new SIPColorScalar(this.data[p]);
+};
+SIPDataScalar.prototype.setColor = function(x, y, color) {
+	var p = y * this.width + x;
+	this.data[p]     = color.getColor();
+};
+SIPDataScalar.prototype.putImageData = function(imagedata, n) {
+	this.setSize(imagedata.width, imagedata.height);
+	if(!n) {
+		n = 0;
+	}
+	var p = 0, i = 0;
+	for(; i < this.data.length; i++) {
+		this.data[i] = imagedata.data[p + n];
+		p += 4;
+	}
+};
+SIPDataScalar.prototype.putImageDataR = function(imagedata) {
+	this.putImageData(imagedata, 0);
+};
+SIPDataScalar.prototype.putImageDataG = function(imagedata) {
+	this.putImageData(imagedata, 1);
+};
+SIPDataScalar.prototype.putImageDataB = function(imagedata) {
+	this.putImageData(imagedata, 2);
+};
+SIPDataScalar.prototype.putImageDataA = function(imagedata) {
+	this.putImageData(imagedata, 3);
+};
+SIPDataScalar.prototype.getImageData = function() {
+	var canvas, context;
+	canvas = document.createElement("canvas");
+	canvas.width  = this.width;
+	canvas.height = this.height;
+	context = canvas.getContext("2d");
+	var imagedata = context.getImageData(0, 0, canvas.width, canvas.height);
+	var p = 0, i = 0;
+	for(; i < this.data.length; i++) {
+		var x = Math.floor(this.data[i]);
+		imagedata.data[p + 0] = x;
+		imagedata.data[p + 1] = x;
+		imagedata.data[p + 2] = x;
+		imagedata.data[p + 3] = 255;
+		p += 4;
+	}
+	return imagedata;
+};
+
+/**
+ * /////////////////////////////////////////////////////////
  * 画像処理用クラス
  * /////////////////////////////////////////////////////////
  */
@@ -232,6 +461,3 @@ SImageProcessing.prototype.setUseBuffer = function(isusebuffer) {
 		}
 	}
 };
-
-var a = new SIPColorPickerText();
-var b = new SIPPixelSelecterRepeat(a, 320, 320);
--- docs/samples/SImageProcessing/main.js
+++ docs/samples/SImageProcessing/main.js
@@ -38,6 +38,47 @@ function test2(panel) {
 	
 	panel.clearChildNodes();
 	
+	// Canvas
+	var canvas = new SCanvas();
+	canvas.setPixelSize(256, 256);
+	canvas.setUnit(SComponent.unittype.PX);
+	canvas.setSize(256, 256);
+	canvas.getContext().fillStyle = "rgb(0, 0, 0)";
+	var size = canvas.getSize();
+	canvas.getContext().fillRect(0, 0, size.width, size.height);
+	
+	panel.put(canvas, SComponent.putype.IN);
+	
+	// ボタン1
+	var button1 = new SButton("RGBA");
+	canvas.put(button1, SComponent.putype.NEWLINE);
+	button1.addListener(function() {
+		var data = new SIPDataRGBA();
+		data.putImageData(canvas.getImageData());
+		var i = 0;
+		for(i = 0; i < 100; i++) {
+			var x = Math.floor(Math.random() * data.width);
+			var y = Math.floor(Math.random() * data.height);
+			data.setColor(x, y, new SIPColorRGBA([255, 255, 255, 255]));
+		}
+		canvas.setImageData(data.getImageData());
+	});
+	
+	// ボタン2
+	var button2 = new SButton("Scaler");
+	button1.put(button2, SComponent.putype.RIGHT);
+	button2.addListener(function() {
+		var data = new SIPDataScalar();
+		data.putImageData(canvas.getImageData());
+		var i = 0;
+		for(i = 0; i < 100; i++) {
+			var x = Math.floor(Math.random() * data.width);
+			var y = Math.floor(Math.random() * data.height);
+			data.setColor(x, y, new SIPColorScalar(255));
+		}
+		canvas.setImageData(data.getImageData());
+	});
+	
 };
 
 
@@ -52,16 +93,19 @@ function test2(panel) {
 	var label = new SLabel("SImageProcessing のテストです");
 	mainpanel.put(label, SComponent.putype.IN);
 	
-	var combobox = new SComboBox(["画像ファイルの読み込み", "test2"]);
+	var combobox_type = [
+		"画像ファイルの読み込み", "データの読み書き"
+	];
+	var combobox = new SComboBox(combobox_type);
 	combobox.setWidth(20);
 	label.put(combobox, SComponent.putype.NEWLINE);
 	
 	combobox.addListener(function () {
 		var item = combobox.getSelectedItem();
-		if(item === "画像ファイルの読み込み") {
+		if(item === combobox_type[0]) {
 			testFileLoad(testpanel);
 		}
-		else if(item === "test2") {
+		else if(item === combobox_type[1]) {
 			test2(testpanel);
 		}
 	});
@@ -69,6 +113,7 @@ function test2(panel) {
 	var testpanel = new SPanel();
 	mainpanel.put(testpanel, SComponent.putype.NEWLINE);
 	
-	testFileLoad(testpanel);
+	combobox.setSelectedItem(combobox_type[1]);
+	test2(testpanel);
 }
 
