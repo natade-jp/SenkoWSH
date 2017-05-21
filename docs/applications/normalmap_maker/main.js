5c69baf8fd632963f266b5bd5e59ce77f30f6ba3
natade-jp <natade3@gmail.com>
2017-05-22 01:32:16 +0900

    とりあえずノーマルマップを表示できるようにした

--- docs/applications/normalmap_maker/main.js
+++ docs/applications/normalmap_maker/main.js
@@ -11,14 +11,71 @@ imagedata_resized.setSelecter(SIData.selectertype.REPEAT);
 imagedata_resized.setInterPolation(SIData.interpolationtype.BICUBIC_SOFT);
 var setting_width = 0;
 var setting_height = 0;
+var setting_denoise = 0;
 
+var fileloadbtn = new SFileLoadButton("読み込む");
+var filesavebtn = new SFileSaveButton("保存する");
 
+var canvas_readsample = new SCanvas();
+canvas_readsample.setUnit(SComponent.unittype.PX);
+
+var canvas_normalsample = new SCanvas();
+canvas_normalsample.setUnit(SComponent.unittype.PX);
+
+var redrawReadSample = function() {
+	var src_size = imagedata_readgray.width * imagedata_readgray.height;
+	var dst_size = imagedata_readgray.width * imagedata_readgray.height;
+	imagedata_resized.setSize(setting_width, setting_height);
+	
+	// 入力画像が小さいのであれば、入力画像にデノイズ処理
+	if(src_size < dst_size) {
+		var imagedata_denoised = new SIDataY(imagedata_readgray);
+		imagedata_denoised.setSelecter(SIData.selectertype.REPEAT);
+		imagedata_denoised.setInterPolation(SIData.interpolationtype.BICUBIC_SOFT);
+		if(setting_denoise > 0) {
+			var sizelist	= [0, 3, 3, 5];
+			var powerlist	= [0, 0.8, 1.0, 1.0];
+			imagedata_denoised.filterBilateral(
+				sizelist[setting_denoise],
+				powerlist[setting_denoise]
+			);
+		}
+		imagedata_resized.drawSIData(imagedata_denoised, 0, 0, setting_width, setting_height);
+	}
+	// 出力画像が小さいのであれば、出力画像にデノイズ処理
+	else {
+		imagedata_resized.drawSIData(imagedata_readgray, 0, 0, setting_width, setting_height);
+		if(setting_denoise > 0) {
+			var sizelist	= [0, 3, 3, 5];
+			var powerlist	= [0, 0.8, 1.0, 1.0];
+			imagedata_resized.filterBilateral(
+				sizelist[setting_denoise],
+				powerlist[setting_denoise]
+			);
+		}
+	}
+	canvas_readsample.setPixelSize(setting_width, setting_height);
+	canvas_readsample.setSize(setting_width, setting_height);
+	canvas_readsample.setImageData(imagedata_resized.getImageData());
+	redrawNormalMap();
+};
+
+var redrawNormalMap = function() {
+	
+	canvas_normalsample.setPixelSize(setting_width, setting_height);
+	canvas_normalsample.setSize(setting_width, setting_height);
+	canvas_normalsample.setImageData(imagedata_resized.getNormalMap().getImageData());
+	
+	filesavebtn.setURL(canvas_normalsample.toDataURL());
+};
+
+	
 function makeInputPanel() {
 	
 	var panel = new SPanel();
 	panel.putMe("scomponent_input", SComponent.putype.IN);
 	
-	var l_disc = new SLabel("画像ファイルを読み込んでください");
+	var l_disc = new SLabel("入力画像の設定");
 	l_disc.putMe(panel, SComponent.putype.IN);
 	
 	var sizelist = [8, 16, 32, 64, 128, 256, 512];
@@ -26,35 +83,33 @@ function makeInputPanel() {
 	c_width.setSelectedItem(512);
 	var c_height = new SComboBox(sizelist);
 	c_height.setSelectedItem(512);
+	var denoiselist = [0, 1, 2, 3];
+	var c_denoise = new SComboBox(denoiselist);
+	c_denoise.setSelectedItem(0);
 	
-	var redraw = function() {
-		setting_width  = parseFloat(c_width.getSelectedItem());
-		setting_height = parseFloat(c_height.getSelectedItem());
-		imagedata_resized.setSize(setting_width, setting_height);
-		imagedata_resized.drawSIData(imagedata_readgray, 0, 0, setting_width, setting_height);
-		canvas_readsample.setPixelSize(setting_width, setting_height);
-		canvas_readsample.setSize(setting_width, setting_height);
-		canvas_readsample.setImageData(imagedata_resized.getImageData());
+	var redrawReadSampleResized = function() {
+		setting_width	= parseFloat(c_width.getSelectedItem());
+		setting_height	= parseFloat(c_height.getSelectedItem());
+		setting_denoise	= parseFloat(c_denoise.getSelectedItem());
+		redrawReadSample();
 	};
+	c_width.addListener(redrawReadSampleResized);
+	c_height.addListener(redrawReadSampleResized);
+	c_denoise.addListener(redrawReadSampleResized);
 	
-	var edit_size = function() {
-		redraw();
-	};
-	c_width.addListener(edit_size);
-	c_height.addListener(edit_size);
-	
-	
-	var l_width  = new SLabel("横幅[px]");
-	var l_height = new SLabel("縦幅[px]");
+	var l_width  = new SLabel("よこ[px]");
 	l_width.putMe (l_disc, SComponent.putype.NEWLINE);
 	c_width.putMe (l_width, SComponent.putype.RIGHT);
-	l_height.putMe(c_width, SComponent.putype.NEWLINE);
+	var l_height = new SLabel("たて[px]");
+	l_height.putMe(c_width, SComponent.putype.RIGHT);
 	c_height.putMe(l_height, SComponent.putype.RIGHT);
+	var l_denoise = new SLabel("きれい");
+	l_denoise.putMe(c_height, SComponent.putype.NEWLINE);
+	c_denoise.putMe(l_denoise, SComponent.putype.RIGHT);
 	
 	// FileLoad
-	var fileloadbtn = new SFileLoadButton("load");
 	fileloadbtn.setFileAccept(SFileLoadButton.fileaccept.image);
-	fileloadbtn.putMe(c_height, SComponent.putype.NEWLINE);
+	fileloadbtn.putMe(c_denoise, SComponent.putype.NEWLINE);
 	fileloadbtn.addListener(function(file) {
 		var canvas_read = new SCanvas();
 		canvas_read.setImage(
@@ -63,8 +118,7 @@ function makeInputPanel() {
 				imagedata_readrgba.putImageData(canvas_read.getImageData());
 				imagedata_readrgba.grayscale();
 				imagedata_readgray.putImageData(imagedata_readrgba);
-				
-				redraw();
+				redrawReadSampleResized();
 			},
 			SCanvas.drawtype.ORIGINAL,
 			true
@@ -72,13 +126,26 @@ function makeInputPanel() {
 	});
 	
 	// Canvas
-	var canvas_readsample = new SCanvas();
-	canvas_readsample.setUnit(SComponent.unittype.PX);
 	canvas_readsample.putMe(fileloadbtn, SComponent.putype.NEWLINE);
 	
 }
 
 function makeEditPanel() {
+	
+	var panel = new SPanel();
+	panel.putMe("scomponent_edit", SComponent.putype.IN);
+	
+	var l_disc = new SLabel("ノーマルマップの調整");
+	l_disc.putMe(panel, SComponent.putype.IN);
+	
+	// FileSave
+	filesavebtn.putMe(l_disc, SComponent.putype.NEWLINE);
+	
+	// Canvas
+	canvas_normalsample.putMe(filesavebtn, SComponent.putype.NEWLINE);
+	
+	
+	
 	
 }
 
@@ -88,6 +155,7 @@ function makeEditPanel() {
 ﻿function main(args) {
 	
 	makeInputPanel();
+	makeEditPanel();
 	
 }
 
