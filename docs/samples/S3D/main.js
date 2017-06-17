31927915a0f104ff37ed9756dc28d376753471b1
natade-jp <natade3@gmail.com>
2017-06-18 00:50:16 +0900

    行列の計算のサンプルを追加

--- docs/samples/S3D/main.js
+++ docs/samples/S3D/main.js
@@ -1,9 +1,30 @@
+/* global System */
+
 ﻿
 ﻿function main(args) {
 	
 	System.out.println("S3D クラスのサンプル");
 	
-	// 準備中
+	var m = new S3Matrix(
+		3, -2, -6, 4,
+		-7, -6, 8, 21,
+		-4, -7, 9, 11,
+		2, -3, -5, 8
+	);
+	
+	System.out.println("行列を作成");
+	System.out.println(m);
+	
+	System.out.println("行列の行列式");
+	System.out.println(m.det());
+	
+	System.out.println("行列の逆行列");
+	System.out.println(m.inverse());
+	
+	System.out.println("行列の掛け算");
+	System.out.println(m.mul(m));
+	
+	
 	
 }
 
